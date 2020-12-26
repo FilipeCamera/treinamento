3125b5fd0993f7143f642abcecbbe0596586630a
Filipe <filipecameracastro@gmail.com>
2020-12-26 14:56:28 -0300

    Feito App

---
+++ src/app.ts
@@ -0,0 +1,22 @@
+import 'reflect-metadata';
+
+import express from 'express';
+
+import routes from './routes';
+
+class App {
+  public app: express.Application;
+
+  constructor(){
+    this.app = express();
+    this.middlewares();
+    this.routes();
+  }
+
+  middlewares(){
+    this.app.use(express.json());
+  }
+  routes(){
+    this.app.use(routes);
+  }
+}
